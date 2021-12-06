83f2d0938ea01d72f35f02489ebd15c17b961bae
nadavskloot <50923728+nadavskloot@users.noreply.github.com>
2021-12-06 16:57:23 -0600

    getting linkedin scraper to same place as the others

--- linkedin_scraping.py
+++ linkedin_scraping.py
@@ -2,6 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.service import Service
 
 from bs4 import BeautifulSoup
@@ -9,7 +12,7 @@ import requests
 import re
 import sys
 import pprint
-
+import time
 
 def setupDriver():
     chrome_options = Options()
@@ -30,34 +33,82 @@ def search(driver, kewWord, location):
     jobInput.send_keys(kewWord)
     jobLocation.clear()
     jobLocation.send_keys(location)
+    base = driver.find_element(By.TAG_NAME, "html")
     jobInput.send_keys(Keys.RETURN) # search
+    waitForRefresh(driver, base)
+    return driver
 
-    return driver.page_source
+def scrape(driver):
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-def scrape(page_source):
-    soup = BeautifulSoup(page_source, 'html.parser')
+    jobsUl = soup.find("ul", attrs={"class": re.compile("jobs-search__results-list")})
+    jobLinks = jobsUl.find_all("a", href=True, attrs={"class": re.compile("base-card__full-link")})
+    print(len(jobLinks))
+    
+    for job in jobLinks:
+        print(job['href'])
+        # base_url = job['href']
+        # base = driver.find_element(By.TAG_NAME, "html")
+        # driver.get(base_url)
+        
+        # waitForRefresh(driver, base)
+        
+        # soup = BeautifulSoup(driver.page_source, "html.parser")
+        time.sleep(2)
+        base_url = job['href']
+        r = requests.get(base_url)
+        soup = BeautifulSoup(r.content, 'html.parser')
+        # print(soup)
+        jobHeader = soup.find("section", attrs={"class": re.compile("top-card-layout")})
+        jobTitle = jobHeader.find("h1", attrs={"class": re.compile("topcard__title")})
+        jobEmployer = jobHeader.find("a", attrs={"class": re.compile("topcard__org-name-link")})
+        # jobLocation = jobHeader.find("a", attrs={"class": re.compile("topcard__org-name-link")})
+        jobLocation = jobHeader.find("span", attrs={"class": "topcard__flavor"}).findNextSibling()
 
-    jobTitles = soup.find_all("h3", attrs={'class': re.compile("base-search-card__title")})
-    jobEmployers = soup.find_all("h4", attrs={'class': re.compile("base-search-card__subtitle")})
-    jobLocations = soup.find_all("span", attrs={'class': re.compile("job-search-card__location")})
+        jobInfo = soup.find("ul", attrs={"class": re.compile("description__job-criteria-list")})
+        
+        jobDescriptionDiv = soup.find("div", attrs={"class": re.compile("show-more-less-htm")})
+
+
+        
+
+        # print(jobHeader)
+        print(jobTitle.string)
+        print(jobEmployer.text.strip())
+        print(jobLocation.text.strip())
+        # print(jobInfo)
+        print()
+
+    # jobTitles = soup.find_all("h3", attrs={'class': re.compile("base-search-card__title")})
+    # jobEmployers = soup.find_all("h4", attrs={'class': re.compile("base-search-card__subtitle")})
+    # jobLocations = soup.find_all("span", attrs={'class': re.compile("job-search-card__location")})
     
 
-    jobs = {}
-    for i in range(len(jobTitles)):
-        job = jobEmployers[i].text.strip() + " - " + jobTitles[i].text.strip()
-        jobs[job] = {
-            "title": jobTitles[i].text.strip(),
-            "employer": jobEmployers[i].text.strip(),
-            'location': jobLocations[i].text.strip()
-        }
-    print()
-    pp = pprint.PrettyPrinter()
-    pp.pprint(jobs)
+    # jobs = {}
+    # for i in range(len(jobTitles)):
+    #     job = jobEmployers[i].text.strip() + " - " + jobTitles[i].text.strip()
+    #     jobs[job] = {
+    #         "title": jobTitles[i].text.strip(),
+    #         "employer": jobEmployers[i].text.strip(),
+    #         'location': jobLocations[i].text.strip()
+    #     }
+    # print()
+    # pp = pprint.PrettyPrinter()
+    # pp.pprint(jobs)
 
+def waitForRefresh(driver, base):
+    try:
+        element = WebDriverWait(driver, 20).until(
+        EC.staleness_of(base)
+    )
+        return
+    except TimeoutException:
+        print("baddd")
+        raise TimeoutError
 
 if __name__ == "__main__":
     keyWord = sys.argv[1]
     location = sys.argv[2]
     driver = setupDriver()
-    page_source = search(driver, keyWord, location)
-    scrape(page_source)
+    driver = search(driver, keyWord, location)
+    scrape(driver)
