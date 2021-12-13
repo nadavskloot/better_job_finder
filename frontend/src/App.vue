e4f6a81b1e758f993fc2040d85c95214c7d416b5
Becka Geleto <beckaberhanu2@gmail.com>
2021-12-12 22:47:36 -0600

    frontend now populates job results section

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,36 +1,47 @@
 <template>
-  <header>
-    <h1 id="page_title">Better Job Finder</h1>
-  </header>
-  <div id="app">
-    <SearchEngine />
-  </div>
+    <div id="App">
+        <header>
+            <h1 id="page_title">Better Job Finder</h1>
+        </header>
+        <SearchEngine />
+    </div>
 </template>
 
 <script>
-import SearchEngine from './components/SearchEngine.vue'
+import SearchEngine from "./components/SearchEngine.vue";
 
 export default {
-  name: 'App',
-  components: {
-    SearchEngine
-  }
-}
+    name: "App",
+    components: {
+        SearchEngine,
+    },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin: 0px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin: 0px;
+    height: 100%;
+}
+
+body {
+    height: 100vh;
+    margin: 0;
+}
+#app > div {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
 }
 
 #page_title {
-  font-weight: 500;
-  font-size: 50px;
-  margin: 0px;
+    font-weight: 500;
+    font-size: 50px;
+    margin: 0px;
 }
 </style>
